cf132abdf8620dc3221863aa62e13ddbe961510a
Akihiro <shiroganseki@gmail.com>
2018-04-07 16:58:56 +0900

    最初のコミット

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Task;
+use Illuminate\Http\Request;
+
+Route::get('/', function () {
+   return view('tasks'); 
+});
+
+Route::post('/task', function () {
+    $validator = Validator::make($request->all(), [
+		'name' => 'required|max:255',
+	]);
+
+	if ($validator->fails()) {
+		return redirect('/')
+			->withInput()
+			->withErrors($validator);
+	}
+
+	// TODO:Add New Task
+
+});
+
+Route::delete('/task/{task}', function () {
+
+});
